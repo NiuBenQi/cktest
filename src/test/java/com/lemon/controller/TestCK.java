61cb4b7730851232e956ab4c65f4133a5fb749eb
niubenqi <niubenqi@58.com>
2020-03-28 10:33:49 +0800

    git命令练习

---
+++ src/test/java/com/lemon/controller/TestCK.java
@@ -0,0 +1,10 @@
+package com.lemon.controller;
+
+/**
+ * @program: cktest
+ * @description
+ * @author: NiuBenQi
+ * @create: 2020-03-26 20:22
+ **/
+public class TestCK {
+}
---
+++ src/test/java/com/lemon/generator/demo.java
@@ -0,0 +1,10 @@
+package com.lemon.generator;
+
+/**
+ * @program: cktest
+ * @description
+ * @author: NiuBenQi
+ * @create: 2020-03-24 22:17
+ **/
+public class demo {
+}
